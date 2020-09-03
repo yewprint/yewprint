892ef1eec091b90ea907dbe9109ed617beae4372
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-09-03 17:14:16 +0200

    Download CSS from npm directly

--- build.sh
+++ build.sh
@@ -6,7 +6,12 @@ else
 	options=()
 fi
 
+if ! [ -f core.tgz ]; then
+	curl -o core.tgz https://registry.npmjs.org/@blueprintjs/core/-/core-3.30.0.tgz
+fi
+
 rm -fR static/.gitignore static/*
+tar xvzf core.tgz -C static --wildcards \*.css --transform='s/.*\///'
 wasm-pack build --no-typescript --target web --out-name wasm --out-dir ./static "${options[@]}" "$@"
 rc=$?
 rm -fR static/{.gitignore,package.json}
