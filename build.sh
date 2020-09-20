4ef1cd592760b1444d26b935a86d5921409e4e7e
Esteban Borai <estebanborai@gmail.com>
2020-09-20 14:59:47 -0300

    fix: support for macos OOTB

--- build.sh
+++ build.sh
@@ -11,10 +11,20 @@ if ! [ -f core.tgz ]; then
 fi
 
 mkdir -p static
+
 rm -fR static/.gitignore static/*
-tar xvzf core.tgz -C static --wildcards \*.css --transform='s/.*\///'
+
+# decompress the tar file but extract the `.css` files only
+# find every file in the nested directories and move it behind `./static`
+# finally remove every directory behind `./static`
+bsdtar xvzf core.tgz -C static \*.css &&
+	find ./static -mindepth 2 -type f -print -exec mv {} ./static \; &&
+	find ./static -mindepth 1 -type d -print -exec rm -rf {} \;
+
 wasm-pack build --no-typescript --target web --out-name wasm --out-dir ./static "${options[@]}" "$@"
+
 rc=$?
+
 rm -fR static/{.gitignore,package.json}
 
 exit $rc
--- dev.sh
+++ dev.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-exec cargo watch -s './build.sh --dev -- --features dev && simple-http-server -i --nocache --cors' -w src
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	# if current OS is `darwin` then avoid running `cargo watch` by default
+	# refer to: https://github.com/passcod/cargo-watch/issues/129
+	./build.sh --dev -- --features dev && simple-http-server -i --nocache --cors
+else
+	exec cargo watch -s './build.sh --dev -- --features dev && simple-http-server -i --nocache --cors' -w src
+fi
