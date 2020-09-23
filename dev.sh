bbd7c9bf921efd1a93c0ccd9032707bfaf52c52a
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-09-23 15:17:36 +0200

    WIP

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec cargo watch -s './build.sh --dev -- --features dev && simple-http-server -i --nocache --cors public/' -w src
+exec cargo watch -s './build.sh --dev -- --features dev && simple-http-server -i --nocache --cors public/' -w src -w static
