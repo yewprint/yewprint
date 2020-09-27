75f7dd5b3e86448e1bf032e52de8b03548cb713e
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-09-27 11:52:33 +0200

    Download CSS

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@ if ! [ -f core.tgz ]; then
 	curl -o core.tgz https://registry.npmjs.org/@blueprintjs/core/-/core-3.30.0.tgz
 fi
 
+if ! [ -f docs-theme.tgz ]; then
+	curl -o docs-theme.tgz https://registry.npmjs.org/@blueprintjs/docs-theme/-/docs-theme-3.7.1.tgz
+fi
+
 # cleanup
 mkdir -p public
 rm -fR public/.gitignore public/*
@@ -22,6 +26,9 @@ cp src/logo.svg public/favicon.svg
 
 # download blueprint css
 tar xzOf core.tgz package/lib/css/blueprint.css > public/blueprint.css
+
+# download blueprint doc css
+tar xzOf docs-theme.tgz package/lib/css/docs-theme.css > public/docs-theme.css
 
 # build
 wasm-pack build --no-typescript --target web --out-name wasm --out-dir ./public "${options[@]}" "$@"
