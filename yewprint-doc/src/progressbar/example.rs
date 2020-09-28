f0a15b80128e3a131b0559b1ad8a3370a98d8ffc
nbrr <nbrr@protonmail.com>
2020-09-29 01:43:12 +0200

    Refine doc example.

--- yewprint-doc/src/progressbar/example.rs
+++ yewprint-doc/src/progressbar/example.rs
@@ -1,24 +1,38 @@
 use yew::prelude::*;
 use yewprint::{ProgressBar, Intent};
 
-pub struct Example {}
+pub struct Example {
+    props: ExampleProps,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct ExampleProps {
+    pub intent: Option<Intent>,
+    pub animate: bool,
+    pub stripes: bool,
+}
 
 impl Component for Example {
     type Message = ();
-    type Properties = ();
+    type Properties = ExampleProps;
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Example {}
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Example { props }
     }
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
         true
     }
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        true
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
     }
     fn view(&self) -> Html {
         html! {
-            <ProgressBar animate=true stripes=true intent=Intent::Warning value=0.3 />
+            <ProgressBar animate=self.props.animate stripes=self.props.stripes intent=self.props.intent value=0.3 />
         }
     }
 }
