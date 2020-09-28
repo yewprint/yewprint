367a8369138df839ff66078ed9a21475c230b4a8
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-09-28 12:40:39 +0200

    WIP
    Forked at: a54a8ffb436f88480aa58838d669db65a8964b5c
    Parent branch: origin/main

--- yewprint-doc/src/callout/mod.rs
+++ yewprint-doc/src/callout/mod.rs
@@ -1,16 +1,32 @@
-use yew::prelude::*;
+mod example;
 
-pub struct CalloutDoc;
+use crate::ExampleContainer;
+use example::*;
+use yew::prelude::*;
+use yewprint::{Intent, Menu, MenuItem, Switch};
+
+pub struct CalloutDoc {
+    callback: Callback<ExampleProps>,
+    state: ExampleProps,
+}
 
 impl Component for CalloutDoc {
-    type Message = ();
+    type Message = ExampleProps;
     type Properties = ();
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        CalloutDoc
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        CalloutDoc {
+            callback: link.callback(|x| x),
+            state: ExampleProps {
+                show_icon: true,
+                intent: None,
+                show_title: true,
+            },
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        self.state = msg;
         true
     }
 
@@ -19,13 +35,94 @@ impl Component for CalloutDoc {
     }
 
     fn view(&self) -> Html {
-        let source = crate::include_example!();
+        let example_props = self.state.clone();
+        let source = crate::include_raw_html!(
+            concat!(env!("OUT_DIR"), "/", file!(), ".html"),
+            "bp3-code-block"
+        );
 
         html! {
             <div>
                 <h1>{"Callout"}</h1>
-                <div>{source}</div>
+                <ExampleContainer
+                    source=source
+                    props=Some(html! {
+                        <CalloutProps
+                            callback={self.callback.clone()}
+                            props=example_props.clone()
+                        />
+                    })
+                >
+                    <Example with example_props />
+                </ExampleContainer>
             </div>
         }
     }
+}
+
+crate::build_example_prop_component! {
+    CalloutProps for ExampleProps =>
+        fn view(&self) -> Html {
+            html! {
+                <div>
+                    <h5>{"Props"}</h5>
+                    <div>
+                        <Switch
+                            onclick=self.update_props(|props| ExampleProps {
+                                show_icon: !props.show_icon,
+                                ..props
+                            })
+                            checked=self.props.show_icon
+                            label="Show/hide icon"
+                        />
+                        <Switch
+                            onclick=self.update_props(|props| ExampleProps {
+                                show_title: !props.show_title,
+                                ..props
+                            })
+                            checked=self.props.show_title
+                            label="Show/hide title"
+                        />
+                        <p>{"Select intent:"}</p>
+                        <Menu>
+                            <MenuItem
+                                onclick=self.update_props(|props| ExampleProps {
+                                    intent: None,
+                                    ..props
+                                })
+                                text=html!{"None"}
+                            />
+                            <MenuItem
+                                onclick=self.update_props(|props| ExampleProps {
+                                    intent: Some(Intent::Primary),
+                                    ..props
+                                })
+                                text=html!{"Primary"}
+                            />
+                            <MenuItem
+                                onclick=self.update_props(|props| ExampleProps {
+                                    intent: Some(Intent::Success),
+                                    ..props
+                                })
+                                text=html!{"Success"}
+                            />
+                            <MenuItem
+                                onclick=self.update_props(|props| ExampleProps {
+                                    intent: Some(Intent::Warning),
+                                    ..props
+                                })
+                                text=html!{"Warning"}
+                            />
+                            <MenuItem
+                                onclick=self.update_props(|props| ExampleProps {
+                                    intent: Some(Intent::Danger),
+                                    ..props
+                                })
+                                text=html!{"Danger"}
+                            />
+                        </Menu>
+                    </div>
+                </div>
+            }
+        }
 }
