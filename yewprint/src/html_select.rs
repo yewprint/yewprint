de0d4f017441840131b1500d0e7eecde54a134f3
Yozhgoor <yozhgoor@outlook.com>
2020-10-02 00:08:03 +0200

    Add a struct to Html_select and a struct to the props

--- yewprint/src/html_select.rs
+++ yewprint/src/html_select.rs
@@ -1,3 +1,23 @@
 use crate::{ConditionalClass, Icon, IconName}
 use yew::prelude::*;
 
+pub struct Html_select {
+    props: props,
+}
+
+pub struct Props {
+    #[prop_or_default]
+    pub fill: ConditionalClass,
+    #[prop_or_default]
+    pub minimal: ConditionalClass,
+    #[prop_or_default]
+    pub disable: ConditionalClass,
+    #[prop_or_default]
+    pub icon: Option<IconName>,
+    #[prop_or_default]
+    pub title: Option<String>,
+    pub children: html::Children
+    #[prop_or_default]
+    // onChange
+    //options
+}
