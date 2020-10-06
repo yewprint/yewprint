57c1fdbaf08cded439b522648a5e277f3a3d77fb
Yozhgoor <yozhgoor@outlook.com>
2020-10-06 16:36:35 +0200

    take the code of the button into html_select/example.rs

---
+++ yewprint-doc/src/html_select/example.rs
@@ -0,0 +1,64 @@
+use yew::prelude::*;
+use yewprint::Button;
+
+pub struct Example {
+    link: ComponentLink<Self>,
+    counter: i64,
+    props: ExampleProps,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct ExampleProps {
+    pub minimal: bool,
+    pub fill: bool,
+}
+
+pub enum Msg {
+    AddOne,
+}
+
+impl Component for Example {
+    type Message = Msg;
+    type Properties = ExampleProps;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Example {
+            counter: 0,
+            link,
+            props,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.counter += 1,
+        }
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <p>{"Counter: "}{self.counter}</p>
+                <div>
+                    <Button
+                        onclick=self.link.callback(|_| Msg::AddOne)
+                        minimal=self.props.minimal
+                        fill=self.props.fill
+                    >
+                        {"Add 1"}
+                    </Button>
+                </div>
+            </div>
+        }
+    }
+}
