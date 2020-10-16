b44bcf60dc8eac26a6cf3a7e9f662a78e3c7417c
Yozhgoor <yozhgoor@outlook.com>
2020-10-16 15:53:47 +0200

    the progressbar select is now working

--- yewprint-doc/src/progressbar/mod.rs
+++ yewprint-doc/src/progressbar/mod.rs
@@ -3,7 +3,7 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::{Intent, Menu, MenuItem, Switch, H1, H5, HtmlSelect};
+use yewprint::{Intent, Switch, H1, H5, HtmlSelect};
 
 pub struct ProgressBarDoc {
     callback: Callback<ExampleProps>,
@@ -92,48 +92,11 @@ crate::build_example_prop_component! {
                                 (Some(Intent::Warning), "Warning".to_string()),
                                 (Some(Intent::Danger), "Danger".to_string()),
                             ]}
+                            onchange=self.update_props(|props, intent| ExampleProps {
+                                intent,
+                                ..props
+                            })
                         />
-                        <Menu>
-                            <MenuItem
-                                onclick=self.update_props(|props, _| ExampleProps {
-                                    intent: None,
-                                    ..props
-                                })
-                                text=html!{"None"}
-                            />
-                            <MenuItem
-                                onclick=self.update_props(|props, _| ExampleProps {
-                                    intent: Some(Intent::Primary),
-                                    ..props
-                                })
-                                text=html!{"Primary"}
-                                intent=Intent::Primary
-                            />
-                            <MenuItem
-                                onclick=self.update_props(|props, _| ExampleProps {
-                                    intent: Some(Intent::Success),
-                                    ..props
-                                })
-                                text=html!{"Success"}
-                                intent=Intent::Success
-                            />
-                            <MenuItem
-                                onclick=self.update_props(|props, _| ExampleProps {
-                                    intent: Some(Intent::Warning),
-                                    ..props
-                                })
-                                text=html!{"Warning"}
-                                intent=Intent::Warning
-                            />
-                            <MenuItem
-                                onclick=self.update_props(|props, _| ExampleProps {
-                                    intent: Some(Intent::Danger),
-                                    ..props
-                                })
-                                text=html!{"Danger"}
-                                intent=Intent::Danger
-                            />
-                        </Menu>
                     </div>
                 </div>
             }
