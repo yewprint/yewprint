6f2750902fd238a33304abb245f4c0fd9cf47f29
yozhgoor <yozhgoor@outlook.com>
2020-11-03 21:44:27 +0100

    Create an example to the yewprint doc

---
+++ yewprint-doc/src/numeric_input/example.rs
@@ -0,0 +1,47 @@
+use yew::prelude::*;
+use yewprint::NumericInput;
+
+pub struct Example {
+    link: ComponentLink<Self>,
+    props: ExampleProps,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct ExampleProps {
+    pub fill: bool,
+    pub disabled: bool,
+    pub large: bool,
+}
+
+impl Component for Example {
+    type Message = ();
+    type Properties = ExampleProps;
+
+    fn create(props: Self::Properties, link: Component<Self>) -> Self {
+        Example {
+            link,
+            props,
+        }
+    }
+
+    fn update(&mut self) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <NumericInput />
+            </div>
+        }
+    }
+}
