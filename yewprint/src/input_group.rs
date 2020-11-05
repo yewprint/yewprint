d144efffe93b59d2437d5b5426ea1a650c79c94e
yozhgoor <yozhgoor@outlook.com>
2020-11-05 18:13:22 +0100

    Add the input group component

---
+++ yewprint/src/input_group.rs
@@ -0,0 +1,55 @@
+use boolinator::Boolinator;
+use yew::prelude::*;
+
+pub struct InputGroup {
+    props: InputGroupProps,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct InputGroupProps {
+    #[prop_or_default]
+    pub disabled: bool,
+    #[prop_or_default]
+    pub large: bool,
+    #[prop_or_default]
+    pub small: bool,
+    #[prop_or_default]
+    pub children: html::Children,
+}
+
+impl Component for InputGroup {
+    type Message = ();
+    type Properties = InputGroupProps;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        self { props }
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div
+                class=(
+                    "bp3-input-group",
+                    self.props.disabled.as_some("bp3-disabled"),
+                    self.props.large.as_some("bp3-large"),
+                    self.props.small.as_some("bp3-small"),
+                )
+            >
+                {self.props.children.clone()}
+            </div>
+        }
+    }
+}
