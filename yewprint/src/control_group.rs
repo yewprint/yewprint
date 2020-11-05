8e66adcd95a8e5d4813ef4e56e52dfec1351a518
yozhgoor <yozhgoor@outlook.com>
2020-11-05 19:38:05 +0100

    Create the controlGroup component

---
+++ yewprint/src/control_group.rs
@@ -0,0 +1,49 @@
+use boolinator::Boolinator;
+use yew::prelude::*;
+
+pub struct ControlGroup {
+    props: ControlGroupProps,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct ControlGroupProps {
+    #[prop_or_default]
+    pub fill: bool,
+    #[prop_or_default]
+    pub vertical: bool,
+}
+
+impl Component for ControlGroup {
+    type Message = ();
+    type Properties = ControlGroupProps;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self { props }
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div
+                class=(
+                    "bp3-control-group",
+                    self.props.fill.as_some("bp3-fill"),
+                    self.props.vertical.as_some("bp3-vertical"),
+                )
+            >
+            </div>
+        }
+    }
+}
