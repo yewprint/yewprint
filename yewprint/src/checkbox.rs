92102a5df38bfb5b842c0c9ee2946fb5904f529c
yozhgoor <yozhgoor@outlook.com>
2021-01-09 18:01:04 +0100

    Create checkbox component and example

---
+++ yewprint/src/checkbox.rs
@@ -0,0 +1,69 @@
+use boolinator::Boolinator;
+use yew::prelude::*;
+
+pub struct Checkbox {
+    props: Props,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct Props {
+    #[prop_or_default]
+    pub disabled: bool,
+    #[prop_or_default]
+    pub inline: bool,
+    #[prop_or_default]
+    pub large: bool,
+    #[prop_or_default]
+    pub checked: bool,
+    #[prop_or_default]
+    pub onchange: Callback<MouseEvent>,
+    #[prop_or_default]
+    pub label: yew::virtual_dom::VNode,
+}
+
+impl Component for Checkbox {
+    type Message = ();
+    type Properties = Props;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self { props }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <label
+                class=(
+                    "bp3-control bp3-checkbox",
+                    self.props.disabled.as_some("bp3-disabled"),
+                    self.props.inline.as_some("bp3-inline"),
+                    self.props.large.as_some("bp3-large")
+                )
+            >
+                <input
+                    type="checkbox"
+                    checked={self.props.checked}
+                    onclick={self.props.onchange.clone()}
+                    disabled=self.props.disabled
+                />
+                <span
+                    class="bp3-control-indicator"
+                >
+                </span>
+                {self.props.label.clone()}
+            </label>
+        }
+    }
+}
