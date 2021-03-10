098ba44e026c4a191072487d379e9b2ed78519be
yozhgoor <yozhgoor@outlook.com>
2021-03-10 02:02:50 +0100

    WIP

--- yewprint/src/spinner.rs
+++ yewprint/src/spinner.rs
@@ -1,6 +1,12 @@
 use crate::Intent;
 use yew::prelude::*;
 
+pub const R: i32 = 45;
+// pub const SPINNER_TRACK
+pub const PATH_LENGTH: i32 = 280;
+pub const MIN_SIZE: i32 = 4;
+pub const MIN_STROKE_WIDTH: i32 = 16;
+
 pub struct Spinner {
     props: SpinnerProps,
 }
@@ -38,8 +44,28 @@ impl Component for Spinner {
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <p>{"Hello, world!"}</p>
+            <div
+                class=classes!(
+                    "bp3-spinner",
+                )
+            >
+                <div
+                    class=classes!("bp3-spinner-animation")
+                >
+                    <svg
+                        width=self.props.size
+                        height=self.props.size
+                        // stroke_width
+                        // view_box
+                    >
+                        <path
+                            class=classes!("bp3-spinner-track")
+                        />
+                        <path
+                            class=classes!("bp3-spinner-head")
+                        />
+                    </svg>
+                </div>
             </div>
         }
     }
