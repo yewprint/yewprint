d3c70b615870d3c1d5b4cf8b20d078b6da752d6e
yozhgoor <yozhgoor@outlook.com>
2021-03-19 16:13:21 +0100

    We print something, but its not a victory

--- yewprint/src/spinner.rs
+++ yewprint/src/spinner.rs
@@ -1,5 +1,5 @@
 use crate::Intent;
-// use std::cmp::{min};
+use std::cmp::min;
 use yew::prelude::*;
 
 pub const R: i32 = 45;
@@ -9,10 +9,23 @@ pub const STROKE_WIDTH: i32 = 4;
 pub const MIN_STROKE_WIDTH: i32 = 16;
 pub const SIZE_SMALL: i32 = 20;
 pub const SIZE_STANDARD: i32 = 50;
-pub const SIZE_LARGE: i32 = 100;
+pub const SIZE_LARGE: i32 = 50;
 
 pub struct Spinner {
     props: SpinnerProps,
+}
+
+#[derive(Clone, PartialEq)]
+pub enum Size {
+    Small,
+    Standard,
+    Large,
+}
+
+impl Default for Size {
+    fn default() -> Self {
+        Size::Standard
+    }
 }
 
 #[derive(Clone, PartialEq, Properties)]
@@ -22,7 +35,7 @@ pub struct SpinnerProps {
     #[prop_or_default]
     pub class: Classes,
     #[prop_or_default]
-    pub size: i32,
+    pub size: Size,
     #[prop_or_default]
     pub value: Option<f32>,
 }
@@ -51,17 +64,22 @@ impl Component for Spinner {
     fn view(&self) -> Html {
         let default_value = 0.25;
         let path_length_float = PATH_LENGTH as f32;
-        // let stroke_width =
-        //     cmp::min(MIN_STROKE_WIDTH, (STROKE_WIDTH * SIZE_LARGE) / size);
-        /* fn get_view_box(stroke_width: i32) -> String {
+        let size = match self.props.size {
+            Size::Small => SIZE_SMALL,
+            Size::Standard => SIZE_STANDARD,
+            Size::Large => SIZE_LARGE,
+        };
+        let stroke_width = min(MIN_STROKE_WIDTH, (STROKE_WIDTH * SIZE_LARGE) / size);
+
+        fn get_view_box(stroke_width: i32) -> String {
             let radius = R + stroke_width / 2;
-            let view_box_x = (50 - radius).to_fixed(2);
-            let view_box_width = (radius * 2).to_fixed(2);
+            let view_box_x = 50 - radius;
+            let view_box_width = radius * 2;
             format!(
                 "{} {} {} {}",
                 view_box_x, view_box_x, view_box_width, view_box_width,
             )
-        };*/
+        };
         let spinner_track: String = format!(
             "M 50,50 m 0,-{R} a {R},{R} 0 1 1 0,{R2} a {R},{R} 0 1 1 0,-{R2}",
             R = 45,
@@ -86,10 +104,10 @@ impl Component for Spinner {
                     class=classes!("bp3-spinner-animation")
                 >
                     <svg
-                        width=self.props.size
-                        height=self.props.size
-                        // stroke_width=stroke_width.to_fixed(2)
-                        // view_box= self.get_view_box(stroke_width)
+                        width=size
+                        height=size
+                        stroke_width=stroke_width
+                        view_box= get_view_box(stroke_width)
                     >
                         <path
                             class=classes!("bp3-spinner-track")
