edf172cd03f604eb81dff07d2aefddf9c286ec8c
yozhgoor <yozhgoor@outlook.com>
2021-03-19 18:33:20 +0100

    Fix Clippy

--- yewprint-doc/src/spinner/example.rs
+++ yewprint-doc/src/spinner/example.rs
@@ -36,7 +36,7 @@ impl Component for Example {
         html! {
             <div>
                 <Spinner
-                    size=self.props.size.clone()
+                    size=self.props.size
                     intent=self.props.intent
                 />
             </div>
