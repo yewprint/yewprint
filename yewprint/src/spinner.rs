b3372697eabd581450643e54c87c368865bc6ac4
yozhgoor <yozhgoor@outlook.com>
2021-03-19 12:36:21 +0100

    WIP

--- yewprint/src/spinner.rs
+++ yewprint/src/spinner.rs
@@ -2,7 +2,8 @@ use crate::Intent;
 use yew::prelude::*;
 
 pub const R: i32 = 45;
-pub const SPINNER_TRACK: i32 = 0; // `M 50,50 m 0,-${R} a ${R},${R} 0 1 1 0,${R * 2} a ${R},${R} 0 1 1 0,-${R * 2}`;
+pub const SPINNER_TRACK: &str =
+    "M 50,50 m 0,-${R} a ${R},${R} 0 1 1 0,${R * 2} a ${R},${R} 0 1 1 0,-${R * 2}";
 pub const PATH_LENGTH: i32 = 280;
 pub const MIN_SIZE: i32 = 10;
 pub const STROKE_WIDTH: i32 = 4;
@@ -51,10 +52,16 @@ impl Component for Spinner {
     }
 
     fn view(&self) -> Html {
+        // let stroke_width =
+        //     Math.min(MIN_STROKE_WIDTH, (STROKE_WIDTH * Spinner.SIZE_LARGE) / size);
+        // let stroke_offset =
+        //    PATH_LENGTH - PATH_LENGTH * (value == null ? 0.25 : clamp(value, 0, 1));
         html! {
             <div
                 class=classes!(
                     "bp3-spinner",
+                    self.props.intent,
+                    self.props.class.clone(),
                 )
             >
                 <div
@@ -63,14 +70,19 @@ impl Component for Spinner {
                     <svg
                         width=self.props.size
                         height=self.props.size
-                        // stroke_width
-                        // view_box
+                        // stroke_width=stroke_width.to_fixed(2)
+                        // view_box= self.get_view_box(stroke_width)
                     >
                         <path
                             class=classes!("bp3-spinner-track")
+                            d=SPINNER_TRACK
                         />
                         <path
                             class=classes!("bp3-spinner-head")
+                            d=SPINNER_TRACK
+                            path_length=PATH_LENGTH
+                            // stroke_dash_array="${PATH_LENGTH} ${PATH_LENGTH}"
+                            // stroke_dash_offset=stroke_offset
                         />
                     </svg>
                 </div>
