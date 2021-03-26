a1e4dfe51e6e34b1ab6fd14f28b32303ee656d6e
yozhgoor <yozhgoor@outlook.com>
2021-03-26 16:37:29 +0100

    Update slider examples

--- yewprint-doc/src/slider/example.rs
+++ yewprint-doc/src/slider/example.rs
@@ -61,6 +61,19 @@ impl Component for Example {
                     max=1.0
                     step_size=0.1
                     value=self.value1
+                    label_values=Some(vec![
+                        (0.0, String::from("0.0")),
+                        (0.1, String::from("0.1")),
+                        (0.2, String::from("0.2")),
+                        (0.3, String::from("0.3")),
+                        (0.4, String::from("0.4")),
+                        (0.5, String::from("0.5")),
+                        (0.6, String::from("0.6")),
+                        (0.7, String::from("0.7")),
+                        (0.8, String::from("0.8")),
+                        (0.9, String::from("0.9")),
+                        (1.0, String::from("1.0")),
+                    ])
                     intent=self.props.intent
                     onchange=self.link.callback(|x| Msg::Value1Update(x))
                     vertical=self.props.vertical
@@ -70,6 +83,19 @@ impl Component for Example {
                     max=100
                     step_size=1
                     value=self.value2
+                    label_values=Some(vec![
+                        (0, String::from("0%")),
+                        (10, String::from("10%")),
+                        (20, String::from("10%")),
+                        (30, String::from("10%")),
+                        (40, String::from("10%")),
+                        (50, String::from("10%")),
+                        (60, String::from("10%")),
+                        (70, String::from("10%")),
+                        (80, String::from("10%")),
+                        (90, String::from("10%")),
+                        (100, String::from("100%")),
+                    ])
                     intent=self.props.intent
                     onchange=self.link.callback(|x| Msg::Value2Update(x))
                     vertical=self.props.vertical
