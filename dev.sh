4b705f5d3f120b560ba19f133a362184e722f5de
yozhgoor <yozhgoor@outlook.com>
2021-04-06 16:27:20 +0200

    Change to made compiling by ssh effective

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec cargo watch -s './build.sh --dev && simple-http-server -i --nocache --cors public/' -i /public
+exec cargo watch -s './build.sh --dev && simple-http-server -i --nocache --cors public/' -i public
