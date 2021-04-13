2add1a40f9d9efc9d56c3ad199fc473668f609de
Yozhgoor <yozhgoor@outlook.com>
2021-04-13 13:19:13 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use anyhow::Result;
+use std::process::Command;
 use structopt::StructOpt;
+use wasm_run::prelude::*;
 
-#[wasm_run::main("yewprint-doc")]
+#[wasm_run::main("yewprint-doc", pre_build = pre_build)]
 #[derive(StructOpt, Debug)]
 enum Cli {}
+
+fn pre_build(_args: &DefaultBuildArgs, profile: BuildProfile, command: &mut Command) -> Result<()> {
+    match profile {
+        BuildProfile::Profiling | BuildProfile::Release => {
+            command.args(&["--features", "wee_alloc"]);
+        }
+        BuildProfile::Dev => {
+            command.args(&["--features", "console_error_panic_hook"]);
+        }
+    }
+
+    Ok(())
+}
