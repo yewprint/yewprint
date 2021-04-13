fea5f0a000f3a3399e3176a894fb94783e37930c
Yohan Boogaert <yozhgoor@outlook.com>
2021-04-13 16:23:12 +0200

    Use wasm-run to build yewprint (#104)

--- yewprint-doc/src/lib.rs
+++ yewprint-doc/src/lib.rs
@@ -49,8 +49,16 @@ macro_rules! include_raw_html {
     }};
 }
 
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen::prelude::wasm_bindgen(start)]
 pub fn run_app() -> Result<(), wasm_bindgen::JsValue> {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
     yew::start_app::<app::App>();
 
     Ok(())
