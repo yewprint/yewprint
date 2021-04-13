ea9e92c1f3f73a744f0155333b50e63916fe49d8
Yohan Boogaert <yozhgoor@outlook.com>
2021-04-13 16:23:12 +0200

    Use wasm-run to build yewprint (#104)

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![allow(clippy::unnecessary_wraps)]
+
+use anyhow::Result;
+use std::fs;
+use std::process::Command;
+use structopt::StructOpt;
+use wasm_run::prelude::*;
+
+#[wasm_run::main("yewprint-doc", pre_build)]
+#[derive(StructOpt, Debug)]
+enum Cli {}
+
+fn pre_build(_args: &DefaultBuildArgs, profile: BuildProfile, cargo: &mut Command) -> Result<()> {
+    match profile {
+        BuildProfile::Profiling | BuildProfile::Release => {
+            cargo.args(&["--features", "wee_alloc"]);
+        }
+        BuildProfile::Dev => {
+            cargo.args(&["--features", "console_error_panic_hook"]);
+        }
+    }
+    fs::copy("yewprint-doc/src/logo.svg", "build/favicon.svg")?;
+
+    Ok(())
+}
