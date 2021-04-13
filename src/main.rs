5947ef8e02777d172d25334d5bc29e5ed350fd52
Yozhgoor <yozhgoor@outlook.com>
2021-04-13 13:25:57 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+#![allow(clippy::unnecessary_wraps)]
+
 use anyhow::Result;
 use std::process::Command;
 use structopt::StructOpt;
 use wasm_run::prelude::*;
 
-#[wasm_run::main("yewprint-doc", pre_build = pre_build)]
+#[wasm_run::main("yewprint-doc", pre_build)]
 #[derive(StructOpt, Debug)]
 enum Cli {}
 
-fn pre_build(_args: &DefaultBuildArgs, profile: BuildProfile, command: &mut Command) -> Result<()> {
+fn pre_build(_args: &DefaultBuildArgs, profile: BuildProfile, cargo: &mut Command) -> Result<()> {
     match profile {
         BuildProfile::Profiling | BuildProfile::Release => {
-            command.args(&["--features", "wee_alloc"]);
+            cargo.args(&["--features", "wee_alloc"]);
         }
         BuildProfile::Dev => {
-            command.args(&["--features", "console_error_panic_hook"]);
+            cargo.args(&["--features", "console_error_panic_hook"]);
         }
     }
 
