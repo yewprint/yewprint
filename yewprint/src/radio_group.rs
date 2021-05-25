79fb3da437d44713793d6f22826b49c8c03b28f7
Yozhgoor <yozhgoor@outlook.com>
2021-05-25 16:38:54 +0200

    After review

--- yewprint/src/radio_group.rs
+++ yewprint/src/radio_group.rs
@@ -1,24 +1,28 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use yew::prelude::*;
 
-pub struct RadioGroup<T: Clone + PartialEq + 'static> {
+pub struct RadioGroup<T: Clone + PartialEq + Hash + 'static> {
     props: RadioGroupProps<T>,
 }
 
 #[derive(Clone, PartialEq, Properties)]
-pub struct RadioGroupProps<T: Clone + PartialEq + 'static> {
+pub struct RadioGroupProps<T: Clone + PartialEq + Hash + 'static> {
     #[prop_or_default]
     pub label: Option<yew::virtual_dom::VNode>,
     #[prop_or_default]
     pub label_class: Option<String>,
     #[prop_or_default]
-    pub children: Vec<T>,
+    pub option_children: Vec<(T, String)>,
     #[prop_or_default]
     pub name: String,
     #[prop_or_default]
-    pub selected_value: Option<T>,
+    pub value: Option<T>,
+    #[prop_or_default]
+    pub class: Classes,
 }
 
-impl<T: Clone + PartialEq + 'static> Component for RadioGroup<T>
+impl<T: Clone + PartialEq + Hash + 'static> Component for RadioGroup<T>
 where
     yew::Classes: From<T>,
 {
@@ -43,13 +47,33 @@ where
     }
 
     fn view(&self) -> Html {
+        let option_children = self
+            .props
+            .option_children
+            .iter()
+            .map(|(value, name)| {
+                let selected = self
+                    .props
+                    .value
+                    .as_ref()
+                    .map(|x| value == x)
+                    .unwrap_or_default();
+                let value = {
+                    let mut hasher = DefaultHasher::new();
+                    value.hash(&mut hasher);
+                    hasher.finish()
+                };
+
+                html! {
+                    <input name="group" type="radio" value=value />
+                }
+            })
+            .collect::<Html>();
+
         html! {
             <div
                 class=classes!(
                     "bp3-radio-group",
-                    self.props.name.clone(),
-                    self.props.children.clone(),
-                    self.props.selected_value.clone()
                 )
             >
             {
