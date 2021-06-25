2e240fdb0758001caa519b547e6e209601aff6ee
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-06-25 15:28:41 +0100

    Icons tweaks (#114)

---
+++ yewprint-doc/src/logo.rs
@@ -0,0 +1,37 @@
+use yew::prelude::*;
+
+pub struct Logo {
+    props: LogoProps,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct LogoProps {
+    #[prop_or_default]
+    pub class: Classes,
+}
+
+impl Component for Logo {
+    type Message = ();
+    type Properties = LogoProps;
+
+    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self { props }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        todo!()
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if props != self.props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn view(&self) -> Html {
+        crate::include_raw_html!("logo.svg", &self.props.class.to_string())
+    }
+}
