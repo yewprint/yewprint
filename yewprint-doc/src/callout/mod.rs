bd1db75444ec20752cfd56d2a9d643688656bbef
yozhgoor <yozhgoor@outlook.com>
2021-07-05 14:25:51 +0200

    Add link to the source code of the callout

--- yewprint-doc/src/callout/mod.rs
+++ yewprint-doc/src/callout/mod.rs
@@ -3,7 +3,7 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::{HtmlSelect, Intent, Switch, H1, H5};
+use yewprint::{HtmlSelect, Intent, Switch, Text, H1, H5};
 
 pub struct CalloutDoc {
     callback: Callback<ExampleProps>,
@@ -44,6 +44,13 @@ impl Component for CalloutDoc {
         html! {
             <div>
                 <H1 class=classes!("docs-title")>{"Callout"}</H1>
+                <a
+                    class=classes!("bp3-text-muted")
+                    href="https://github.com/yewprint/yewprint/blob/main/yewprint/src/callout.rs"
+                    target="_blank"
+                >
+                    <Text>{"Go to the source code"}</Text>
+                </a>
                 <ExampleContainer
                     source=source
                     props=Some(html! {
