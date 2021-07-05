5207a02cd633b3838161b99d07d715702aba4421
yozhgoor <yozhgoor@outlook.com>
2021-07-05 15:48:38 +0200

    Add link to the source code of the input group

--- yewprint-doc/src/input_group/mod.rs
+++ yewprint-doc/src/input_group/mod.rs
@@ -3,7 +3,7 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::{Switch, H1, H5};
+use yewprint::{Switch, Text, H1, H5};
 
 pub struct InputGroupDoc {
     callback: Callback<ExampleProps>,
@@ -46,6 +46,13 @@ impl Component for InputGroupDoc {
         html! {
             <div>
                 <H1 class=classes!("docs-title")>{"InputGroup"}</H1>
+                <a
+                    class=classes!("bp3-text-muted")
+                    href="https://github.com/yewprint/yewprint/blob/main/yewprint/src/input_group.rs"
+                    target="_blank"
+                >
+                    <Text>{"Go to the source code"}</Text>
+                </a>
                 <ExampleContainer
                     source=source
                     props=Some(html! {
