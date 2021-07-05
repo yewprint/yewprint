215c08072f8c9a8fc6d81f609d8ee043e9b3e3fb
yozhgoor <yozhgoor@outlook.com>
2021-07-05 16:49:20 +0200

    Add link to the source code of the tabs

--- yewprint-doc/src/tabs/mod.rs
+++ yewprint-doc/src/tabs/mod.rs
@@ -3,7 +3,7 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::{Switch, H1, H5};
+use yewprint::{Switch, Text, H1, H5};
 
 pub struct TabsDoc {
     callback: Callback<ExampleProps>,
@@ -43,6 +43,13 @@ impl Component for TabsDoc {
         html! {
             <div>
                 <H1 class=classes!("docs-title")>{"Tabs"}</H1>
+                <a
+                    class=classes!("bp3-text-muted")
+                    href="https://github.com/yewprint/yewprint/blob/main/yewprint/src/tabs.rs"
+                    target="_blank"
+                >
+                    <Text>{"Go to the source code"}</Text>
+                </a>
                 <div>
                     <ExampleContainer
                         source=source
