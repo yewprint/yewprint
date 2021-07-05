51aaa644265752f2a5189b2929dee817326f9b77
yozhgoor <yozhgoor@outlook.com>
2021-07-05 15:39:23 +0200

    Add the link to the source code of the html select

--- yewprint-doc/src/html_select/mod.rs
+++ yewprint-doc/src/html_select/mod.rs
@@ -3,7 +3,7 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::{Switch, H1, H5};
+use yewprint::{Switch, Text, H1, H5};
 
 pub struct HtmlSelectDoc {
     callback: Callback<ExampleProps>,
@@ -45,6 +45,13 @@ impl Component for HtmlSelectDoc {
         html! {
             <div>
                 <H1 class=classes!("docs-title")>{"HtmlSelect"}</H1>
+                <a
+                    class=classes!("bp3-text-muted")
+                    href="https://github.com/yewprint/yewprint/blob/main/yewprint/src/html_select.rs"
+                    target="_blank"
+                >
+                <Text>{"Go to the source code"}</Text>
+            </a>
                 <div>
                     <ExampleContainer
                         source=source
