150cbe791c435f0f98234db2e9f4a6c662d6395e
yozhgoor <yozhgoor@outlook.com>
2021-07-05 14:18:38 +0200

    Add link to the source code of button group and fmt

--- yewprint-doc/src/button_group/mod.rs
+++ yewprint-doc/src/button_group/mod.rs
@@ -3,7 +3,7 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::{Switch, H1, H5};
+use yewprint::{Switch, Text, H1, H5};
 
 pub struct ButtonGroupDoc {
     callback: Callback<ExampleProps>,
@@ -45,6 +45,13 @@ impl Component for ButtonGroupDoc {
         html! {
             <div>
                 <H1 class=classes!("docs-title")>{"Button Group"}</H1>
+                <a
+                class=classes!("bp3-text-muted")
+                href="https://github.com/yewprint/yewprint/blob/main/yewprint/src/button_group.rs"
+                target="_blank"
+            >
+                <Text>{"Go to the source code"}</Text>
+            </a>
                 <ExampleContainer
                     source=source
                     props=Some(html! {
