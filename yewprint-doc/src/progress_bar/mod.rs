4e2487cec579bc5700f2af84f74b4a0102a8c23b
yozhgoor <yozhgoor@outlook.com>
2021-07-06 18:46:02 +0200

    Customize progress bar

--- yewprint-doc/src/progress_bar/mod.rs
+++ yewprint-doc/src/progress_bar/mod.rs
@@ -44,14 +44,9 @@ impl Component for ProgressBarDoc {
         html! {
             <div>
                 <H1 class=classes!("docs-title")>{"ProgressBar"}</H1>
-                <a
-                    class=classes!("bp3-text-muted")
-                    href="https://github.com/yewprint/yewprint/blob/main\
-                        /yewprint/src/progress_bar.rs"
-                    target="_blank"
-                >
-                    <Text>{"Go to the source code"}</Text>
-                </a>
+                <SourceCodeUrl
+                    url=PROGRESS_BAR_URL
+                />
                 <ExampleContainer
                     source=source
                     props=Some(html! {
@@ -112,7 +107,7 @@ crate::build_example_prop_component! {
 }
 
 crate::build_source_code_component!(
-    _URL,
-    ,
-    check__url
+    PROGRESS_BAR_URL,
+    "https://github.com/yewprint/yewprint/blob/main/yewprint/src/progress_bar.rs",
+    check_progress_bar_url
 );
