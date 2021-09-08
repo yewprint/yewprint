0d10d73d6bd815df50ae7575169c104393e2043a
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-09-08 14:00:02 +0100

    Hotfix for previous commit

--- yewprint/src/tabs.rs
+++ yewprint/src/tabs.rs
@@ -153,7 +153,7 @@ impl<T: Clone + PartialEq + Hash + 'static> Component for Tabs<T> {
                             <div
                                 class=classes!(
                                     "bp3-tab-panel",
-                                    selected.then(|| props.panel_class.clone()).flatten(),
+                                    selected.then(|| props.panel_class.clone()),
                                 )
                                 aria-labelledby=title_id.to_string()
                                 aria-hidden=(!selected).then(|| "true")
