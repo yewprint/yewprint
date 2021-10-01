669c30fa226714c10f94ea22df76972e250bc1e1
Yozhgoor <yozhgoor@outlook.com>
2021-10-01 20:13:14 +0200

    Use a state `input` and a prop `value` instead of convert the string value each time

--- yewprint/src/numeric_input.rs
+++ yewprint/src/numeric_input.rs
@@ -1,21 +1,38 @@
 use crate::{Button, ButtonGroup, ControlGroup, IconName, InputGroup, Intent};
 use std::fmt::Display;
 use std::ops::{Add, Sub};
+use std::process::Output;
 use std::str::FromStr;
 use yew::prelude::*;
 
-pub struct NumericInput<T: Add + Clone + Display + FromStr + PartialEq + PartialOrd + Sub + 'static>
-where
+pub struct NumericInput<
+    T: Add<Output = T>
+        + Clone
+        + Display
+        + FromStr
+        + PartialEq
+        + PartialOrd
+        + Sub<Output = T>
+        + 'static,
+> where
     <T as Sub>::Output: ToString,
     <T as Add>::Output: ToString,
 {
     props: NumericInputProps<T>,
     link: ComponentLink<Self>,
+    input: String,
 }
 
 #[derive(Clone, PartialEq, Properties)]
 pub struct NumericInputProps<
-    T: Add + Clone + Display + FromStr + PartialEq + PartialOrd + Sub + 'static,
+    T: Add<Output = T>
+        + Clone
+        + Display
+        + FromStr
+        + PartialEq
+        + PartialOrd
+        + Sub<Output = T>
+        + 'static,
 > where
     <T as Sub>::Output: ToString,
     <T as Add>::Output: ToString,
@@ -35,8 +52,10 @@ pub struct NumericInputProps<
     pub min_value: T,
     pub max_value: T,
     #[prop_or_default]
-    pub value: String,
-    pub range: T,
+    pub value: Option<T>,
+    pub increment: T,
+    #[prop_or_default]
+    pub onchange: Callback<ChangeData>,
 }
 
 pub enum Msg {
@@ -47,8 +66,16 @@ pub enum Msg {
     Noop,
 }
 
-impl<T: Add + Clone + Display + FromStr + PartialEq + PartialOrd + Sub + 'static> Component
-    for NumericInput<T>
+impl<
+        T: Add<Output = T>
+            + Clone
+            + Display
+            + FromStr
+            + PartialEq
+            + PartialOrd
+            + Sub<Output = T>
+            + 'static,
+    > Component for NumericInput<T>
 where
     <T as Sub>::Output: ToString,
     <T as Add>::Output: ToString,
@@ -57,48 +84,61 @@ where
     type Properties = NumericInputProps<T>;
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { props, link }
+        Self {
+            props,
+            link,
+            input: Default::default(),
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        let max_value = self.props.max_value.clone();
+        let min_value = self.props.min_value.clone();
+        let increment = self.props.increment.clone();
+
         match msg {
             Msg::AddEntry => {
-                Msg::UpdateValue(self.props.value.clone());
-                true
+                if let Ok(num) = self.input.trim().parse::<T>() {
+                    self.props.value = Some(num);
+                    true
+                } else {
+                    self.props.value = None;
+                    false
+                }
             }
             Msg::UpdateValue(value) => {
-                if let Ok(_num) = value.trim().parse::<T>() {
-                    self.props.value = value;
+                if let Ok(num) = value.trim().parse::<T>() {
+                    self.props.value = Some(num);
                     true
                 } else {
                     false
                 }
             }
             Msg::Up => {
-                if let Ok(num) = self.props.value.trim().parse::<T>() {
+                if let Some(num) = self.props.value.clone() {
                     if num >= self.props.max_value {
-                        self.props.value = self.props.max_value.to_string();
+                        self.props.value = Some(max_value);
                         true
                     } else {
-                        self.props.value = (num + self.props.range.clone()).to_string();
+                        self.props.value = Some(num + increment);
                         true
                     }
                 } else {
-                    self.props.value = self.props.max_value.to_string();
+                    self.props.value = Some(max_value);
                     true
                 }
             }
             Msg::Down => {
-                if let Ok(num) = self.props.value.trim().parse::<T>() {
+                if let Some(num) = self.props.value.clone() {
                     if num <= self.props.min_value {
-                        self.props.value = self.props.min_value.to_string();
+                        self.props.value = Some(min_value);
                         true
                     } else {
-                        self.props.value = (num - self.props.range.clone()).to_string();
+                        self.props.value = Some(num - increment);
                         true
                     }
                 } else {
-                    self.props.value = self.props.min_value.to_string();
+                    self.props.value = Some(min_value);
                     true
                 }
             }
@@ -126,7 +166,7 @@ where
                     placeholder="Enter a number..."
                     large=self.props.large
                     disabled=self.props.disabled
-                    value=self.props.value.clone()
+                    value=self.input.clone()
                     oninput=self.link.callback(|e: InputData| Msg::UpdateValue(e.value))
                     onkeydown=self.link.callback(|e: KeyboardEvent| {
                         if e.key() == "Enter" { Msg::AddEntry } else { Msg::Noop }
