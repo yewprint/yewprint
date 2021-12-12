783dcc83567073b8111773f6f902b5c23539a397
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-12-12 11:58:51 +0100

    Apply suggestions from code review

--- yewprint/src/switch.rs
+++ yewprint/src/switch.rs
@@ -48,7 +48,7 @@ impl Component for Switch {
     }
 
     fn view(&self) -> Html {
-        let maybe_display_label = move || -> Html {
+        let display_label = {
             if self.props.inner_label.is_some() || self.props.inner_label_checked.is_some() {
                 let inner_label = self.props.inner_label.as_deref().unwrap_or_default();
                 let inner_label_checked = self.props.inner_label_checked.as_ref();
@@ -66,7 +66,9 @@ impl Component for Switch {
                             </div>
                         </div>
                         <div class=classes!("bp3-control-indicator-child")>
-                            <div class=classes!("bp3-switch-inner-text")>{inner_label.to_string()}</div>
+                            <div class=classes!("bp3-switch-inner-text")>
+                                {inner_label.to_string()}
+                            </div>
                         </div>
                     </>
                 }
@@ -96,7 +98,7 @@ impl Component for Switch {
             <span
                 class=classes!("bp3-control-indicator")
             >
-                {maybe_display_label()}
+                {display_label}
             </span>
             {self.props.label.clone()}
             </label>
