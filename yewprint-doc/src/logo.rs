1b2d796add90a393b8230326b0ae67f4eb9a375f
Francois Stephany <francois@tamere.eu>
2022-03-15 03:57:20 +0100

    Logo (doc) FCified

--- yewprint-doc/src/logo.rs
+++ yewprint-doc/src/logo.rs
@@ -1,37 +1,12 @@
 use yew::prelude::*;
 
-pub struct Logo {
-    props: LogoProps,
-}
-
 #[derive(Clone, PartialEq, Properties)]
 pub struct LogoProps {
     #[prop_or_default]
     pub class: Classes,
 }
 
-impl Component for Logo {
-    type Message = ();
-    type Properties = LogoProps;
-
-    fn create(ctx: &Context<Self>) -> Self {
-        Self { props }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        todo!()
-    }
-
-    fn changed(&mut self, ctx: &Context<Self>) -> bool {
-        if props != self.props {
-            self.props = props;
-            true
-        } else {
-            false
-        }
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        crate::include_raw_html!("logo.svg", &self.props.class.to_string())
-    }
+#[function_component(Logo)]
+pub fn logo(props: &LogoProps) -> Html {
+    crate::include_raw_html!("logo.svg", props.class.to_string())
 }
