7b245fbd270df11984219395ff3ec8ff76b04683
Francois Stephany <francois@tamere.eu>
2022-03-17 23:18:08 +0100

    Icon example FCified

--- yewprint-doc/src/icon/example.rs
+++ yewprint-doc/src/icon/example.rs
@@ -1,24 +1,11 @@
 use yew::prelude::*;
 use yewprint::{Icon, IconName};
 
-pub struct Example {}
-
-impl Component for Example {
-    type Message = ();
-    type Properties = ();
-
-    fn create(ctx: &Context<Self>) -> Self {
-        Example {}
-    }
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div>
-                <Icon icon={IconName::Print} />
-            </div>
-        }
+#[function_component(Example)]
+pub fn example() -> Html {
+    html! {
+        <div>
+            <Icon icon={IconName::Print} />
+        </div>
     }
 }
