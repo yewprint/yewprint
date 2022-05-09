b06c5e8d2dcaced1b3b00a259996ddebe084f12e
yozhgoor <yozhgoor@outlook.com>
2022-05-09 17:05:08 +0200

    Add the `update-css` subcommand to xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -10,38 +10,30 @@ use xtask_wasm::{
 
 #[derive(clap::Parser)]
 enum Cli {
-    Dist(Dist),
+    Dist(xtask_wasm::Dist),
     Watch(xtask_wasm::Watch),
     Start(xtask_wasm::DevServer),
-}
-
-#[derive(clap::Parser)]
-struct Dist {
-    /// Force CSS update
-    #[clap(long, short = 'f')]
-    force: bool,
-
-    #[clap(flatten)]
-    base: xtask_wasm::Dist,
+    /// Update Blueprint CSS and docs-theme CSS.
+    UpdateCSS,
 }
 
 fn main() -> Result<()> {
-    let cli: Cli = clap::Parser::parse();
-
     env_logger::builder()
-        .filter(Some("xtask"), log::LevelFilter::Info)
+        .filter_module("xtask", log::LevelFilter::Info)
+        .filter_module("yewprint", log::LevelFilter::Info)
         .init();
+
+    let cli: Cli = clap::Parser::parse();
 
     match cli {
         Cli::Dist(arg) => {
             log::info!("Generating package...");
 
+            download_css(false)?;
+
             let result = arg
-                .base
                 .static_dir_path("yewprint-doc/static")
                 .run("yewprint-doc")?;
-
-            download_css(&result.dist_dir, arg.force)?;
 
             fs::copy(
                 "yewprint-doc/src/logo.svg",
@@ -57,17 +49,17 @@ fn main() -> Result<()> {
         Cli::Start(arg) => {
             arg.arg("dist").start(xtask_wasm::default_dist_dir(false))?;
         }
+        Cli::UpdateCSS => download_css(true)?,
     }
 
     Ok(())
 }
 
-fn download_css(dest: impl AsRef<Path>, force: bool) -> Result<()> {
+fn download_css(force: bool) -> Result<()> {
     let static_path = PathBuf::from("yewprint-doc/static");
     let css_path = static_path.join("blueprint.css");
 
     if force || !css_path.exists() {
-        log::info!("Downloading Blueprint CSS");
         yewprint_css::download_css(&css_path)?;
 
         let version = yewprint_css::download_from_npm_package(
@@ -79,8 +71,6 @@ fn download_css(dest: impl AsRef<Path>, force: bool) -> Result<()> {
         .context("while downloading CSS of @blueprintjs/docs-theme")?;
         log::info!("Docs Theme CSS updated to: {}", version);
     }
-
-    fs::copy(css_path, dest.as_ref().join("blueprint.css"))?;
 
     Ok(())
 }
