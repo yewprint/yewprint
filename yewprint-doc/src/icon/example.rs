385e6dae1d0d0d9c8737c9100b0391cc69031618
yozhgoor <yozhgoor@outlook.com>
2022-09-21 16:09:02 +0200

    Allow to select the icon intent

--- yewprint-doc/src/icon/example.rs
+++ yewprint-doc/src/icon/example.rs
@@ -1,11 +1,19 @@
 use yew::prelude::*;
-use yewprint::{Icon, IconName};
+use yewprint::{Icon, IconName, Intent};
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct ExampleProps {
+    pub intent: Option<Intent>,
+}
 
 #[function_component(Example)]
-pub fn example() -> Html {
+pub fn example(props: &ExampleProps) -> Html {
     html! {
         <div>
-            <Icon icon={IconName::Print} />
+            <Icon
+                icon={IconName::Print}
+                intent={props.intent}
+            />
         </div>
     }
 }
--- yewprint-doc/src/icon/mod.rs
+++ yewprint-doc/src/icon/mod.rs
@@ -3,23 +3,31 @@ mod example;
 use crate::ExampleContainer;
 use example::*;
 use yew::prelude::*;
-use yewprint::H1;
+use yewprint::{HtmlSelect, Intent, H1, H5};
 
-pub struct IconDoc;
+pub struct IconDoc {
+    callback: Callback<ExampleProps>,
+    state: ExampleProps,
+}
 
 impl Component for IconDoc {
-    type Message = ();
+    type Message = ExampleProps;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        IconDoc
+    fn create(ctx: &Context<Self>) -> Self {
+        IconDoc {
+            callback: ctx.link().callback(|x| x),
+            state: ExampleProps { intent: None },
+        }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        self.state = msg;
         true
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
+        let example_props = self.state.clone();
         let source = crate::include_raw_html!(
             concat!(env!("OUT_DIR"), "/", file!(), ".html"),
             "bp3-code-block"
@@ -29,12 +37,48 @@ impl Component for IconDoc {
             <div>
                 <H1 class={classes!("docs-title")}>{"Icon"}</H1>
                 <SourceCodeUrl />
-                <ExampleContainer source={source}>
-                    <Example />
+                <ExampleContainer
+                    source={source}
+                    props={Some(html! {
+                        <IconProps
+                            callback={self.callback.clone()}
+                            example_props={example_props.clone()}
+                        />
+                    })}
+                    >
+                    <Example ..example_props />
                 </ExampleContainer>
             </div>
         }
     }
+}
+
+crate::build_example_prop_component! {
+    IconProps for ExampleProps =>
+        fn view(&self, ctx: &Context<Self>) -> Html {
+            html! {
+                <div>
+                    <H5>{"Props"}</H5>
+                    <div>
+                        <p>{"Select intent:"}</p>
+                        <HtmlSelect<Option<Intent>>
+                            options={vec![
+                                (None, "None".to_string()),
+                                (Some(Intent::Primary), "Primary".to_string()),
+                                (Some(Intent::Success), "Success".to_string()),
+                                (Some(Intent::Warning), "Warning".to_string()),
+                                (Some(Intent::Danger), "Danger".to_string()),
+                            ]}
+                            value={self.example_props.intent}
+                            onchange={self.update_props(ctx, |props, intent| ExampleProps {
+                                intent,
+                                ..props
+                            })}
+                            />
+                    </div>
+                </div>
+            }
+        }
 }
 
 crate::build_source_code_component!();
